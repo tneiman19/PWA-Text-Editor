ff1f3afed54a8634fccec3c2d2bfce9d5678e452
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-05-10 18:22:01 -0400

    configuered webback, service worker, database, install file, and added dependencies to package.json

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,51 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// DONE: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log("Put to the database");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result.value);
+};
+
+// DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log("result", result);
+  return result?.value;
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,33 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// DONE: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Store the triggered events
+  window.deferredPromt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // Remove the hidden class from the button.
+  butInstall.classlist.toggle("hidden", false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// DONE: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const promtEvent = window.defferedPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+  // Show prompt
+  promtEvent.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPromt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
+
+// DONE: Add an handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  // Clear prompt
+  window.defferedPrompt = null;
+});
