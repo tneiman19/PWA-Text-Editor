7222bfc48bdcd0ddeef353126c515addfd278f8d
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-05-10 19:24:00 -0400

    Fixed typos

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -4,24 +4,24 @@ const butInstall = document.getElementById("buttonInstall");
 // DONE: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
-  window.deferredPromt = event;
+  window.deferredPrompt = event;
 
   // Remove the hidden class from the button.
-  butInstall.classlist.toggle("hidden", false);
+  butInstall.classList.toggle("hidden", false);
 });
 
 // DONE: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-  const promtEvent = window.defferedPrompt;
+  const promptEvent = window.defferedPrompt;
 
   if (!promptEvent) {
     return;
   }
   // Show prompt
-  promtEvent.prompt();
+  promptEvent.prompt();
 
   // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPromt = null;
+  window.deferredPrompt = null;
 
   butInstall.classList.toggle("hidden", true);
 });
